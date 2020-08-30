5bca0d718be2d3113a8412ceea1e3cacaeb225e6
Tuwuh S <tuwuhs@gmail.com>
2020-08-29 20:08:56 -0400

    Play with some basic Tensors (and also Complex in Numerics)

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-numerics", from: "0.0.5"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -22,7 +23,10 @@ let package = Package(
             dependencies: []),
         .target(
             name: "Hello", 
-            dependencies: ["HelloSwift"]),
+            dependencies: [
+                "HelloSwift",
+                .product(name: "Numerics", package: "swift-numerics")
+            ]),
         .testTarget(
             name: "HelloSwiftTests",
             dependencies: ["HelloSwift"]),
--- Sources/Hello/main.swift
+++ Sources/Hello/main.swift
@@ -1,4 +1,29 @@
 import HelloSwift
+import TensorFlow
+import Numerics
 
 var h = HelloSwift()
 print("\(h.text) \(h.num)")
+
+var m: Tensor<Float> = [[1, 2], [3, 4]]
+print(m)
+print(m.transposed())
+print(Float.pi * m)
+print(TensorFlow._Raw.matrixInverse(m))
+print(TensorFlow._Raw.matMul(m, m))
+
+var (s, u, v) = m.svd()
+print(s)
+print(u!)
+print(v!)
+
+// Complex numbers tensors not supported yet?
+// var (e, v_): (Tensor<Float>, Tensor<Float>) = TensorFlow._Raw.eig(m)
+// print(e)
+// print(v_)
+
+// var q: Tensor<Float> = TensorFlow._Raw.complex(real: Tensor<Float>(1.0), imag: Tensor<Float>(2.0))
+// print(q)
+
+var w = Complex(1, 2)
+print(w)
